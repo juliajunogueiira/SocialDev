306f039ebc473945ab13df1ab7a74bfc367d859d
juliajunogueiira <nogueirasouzajulia@gmail.com>
2024-01-27 18:59:48 -0300

    Foto e title

--- Index.js
+++ Index.js
@@ -14,10 +14,7 @@ function toggleMode() {
       "Foto da Julia Nogueira sorrindo, pele negra com cabelo cachedo e fundo rosa."
     )
   } else {
-    img.setAttribute(
-      "src",
-      "./assets/profile-3f0f3cd5-b275-4428-882f-070d3292a4e3-1664646248146 1.png"
-    )
+    img.setAttribute("src", "./assets/julia 1.png")
     img.setAttribute(
       "alt",
       "Foto da Julia Nogueira sorrindo, pele negra com cabelo cachedo e fundo rosa."
